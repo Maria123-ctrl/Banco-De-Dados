60bae1834c4b71d7c49408982711c7e8e5a6aa43
Maria <dudisfreitass17@gmail.com>
2025-10-02 13:56:29 -0300

    Atividade de DDL, DML, DQL devconnect

---
+++ Script_Escola/dml-ddevconnect.sql
@@ -0,0 +1,23 @@
+USE tb_ddevconnect;
+
+INSERT INTO tabela_usuario(nome_completo, nome_usuario, email, senha, foto_perfil_url)
+VALUES
+('Eduarda De Costas Santos', 'Doarda_Costa', 'duda@123', '123' 'httpkdkeollskdeie'),
+('Ronaldo dos Santos', 'Ronaldinho', 'roo@134', '133' 'hdlldloekeollskdeie');w
+
+SELECT * FROM tabela_usuario;
+
+INSERT INTO tabela_publicao(descricao, imagem_url,data_publi)
+VALUES('O dia esta lindo!', '0000000@@hepppp', '2025/08/22'),
+('O dia esta feio!', '0012300@@hepppp', '2025/07/22');
+
+SELECT INTO tabela_publicacao;
+
+INSERT INTO tabela_comentario(texto)
+VALUES
+('O dia esta lindo hoje, meu Deus'),
+('O dia esta ridiulo hoje, meu God');
+
+INSERT INTO tabela_seguidor
+
+INSERT INTO tabela_curtida
